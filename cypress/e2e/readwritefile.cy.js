d036e8e04e54b4e5ceb61810c399eeccd4683b3d
Ayush <ayushjain201474@gmail.com>
2023-06-09 03:52:37 +0530

    Writing files using writeFile()

--- cypress/e2e/readwritefile.cy.js
+++ cypress/e2e/readwritefile.cy.js
@@ -4,8 +4,14 @@ it("Testing fixtures", () => {
     cy.log(data.email);
   });
 });
+
 it("read files using ReadFile()", () => {
   cy.readFile("./cypress/fixtures/example.json").then((data) => {
     cy.log(data.name);
   });
+});
+
+it("Writing files using writeFile()", () => {
+  cy.writeFile("sample.txt", "Hello world\n");
+  cy.writeFile("sample.txt", "I am using Cypress for testing", { flag: "a+" });
 });
