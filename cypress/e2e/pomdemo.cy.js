ee31c775a4a970b4d6c74d5be412987f23563151
Ayush <ayushjain201474@gmail.com>
2023-06-09 01:44:36 +0530

    Testing 2 blocks for same functionality

--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -4,10 +4,20 @@ import { LoginPage } from "./pages/LoginPage";
 
 const loginpage = new LoginPage();
 
-it("POM demo", () => {
+//Test with valid credentials
+it("Login test 1", () => {
   cy.visit("https://opensource-demo.orangehrmlive.com/");
 
   loginpage.enterUsername("Admin");
+  loginpage.enterPassword("admin123");
+  loginpage.clickLogin();
+});
+
+//Test with invalid credentials
+it("login test 2", () => {
+  cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+  loginpage.enterUsername("Admin12");
   loginpage.enterPassword("admin123");
   loginpage.clickLogin();
 });
