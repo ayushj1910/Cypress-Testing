1a2c81afa9fc776eb0c8f96d5fd1175067f684db
Ayush <ayushjain201474@gmail.com>
2023-06-09 01:52:04 +0530

    Usage of beforeEach for login blocks

--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -5,21 +5,24 @@ import { LoginPage } from "./pages/LoginPage";
 const loginpage = new LoginPage();
 
 describe("All Login tests", () => {
-  it("Login test 1", () => {
+  //beforeEach helps to run the same command before each testing block
+  beforeEach(() => {
     cy.visit("https://opensource-demo.orangehrmlive.com/");
+  });
 
+  //Test with valid credentials
+
+  it("Login test 1", () => {
     loginpage.enterUsername("Admin");
     loginpage.enterPassword("admin123");
     loginpage.clickLogin();
   });
 
   //Test with invalid credentials
-  it("login test 2", () => {
-    cy.visit("https://opensource-demo.orangehrmlive.com/");
 
+  it("login test 2", () => {
     loginpage.enterUsername("Admin12");
     loginpage.enterPassword("admin123");
     loginpage.clickLogin();
   });
 });
-//Test with valid credentials
