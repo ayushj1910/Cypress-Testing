45314961d330bcc3918c3902e91d982491eff4dd
Ayush <ayushjain201474@gmail.com>
2023-06-09 01:46:38 +0530

    Adding describe function to all tests of same class

--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -4,20 +4,22 @@ import { LoginPage } from "./pages/LoginPage";
 
 const loginpage = new LoginPage();
 
+describe("All Login tests", () => {
+  it("Login test 1", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+    loginpage.enterUsername("Admin");
+    loginpage.enterPassword("admin123");
+    loginpage.clickLogin();
+  });
+
+  //Test with invalid credentials
+  it("login test 2", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+    loginpage.enterUsername("Admin12");
+    loginpage.enterPassword("admin123");
+    loginpage.clickLogin();
+  });
+});
 //Test with valid credentials
-it("Login test 1", () => {
-  cy.visit("https://opensource-demo.orangehrmlive.com/");
-
-  loginpage.enterUsername("Admin");
-  loginpage.enterPassword("admin123");
-  loginpage.clickLogin();
-});
-
-//Test with invalid credentials
-it("login test 2", () => {
-  cy.visit("https://opensource-demo.orangehrmlive.com/");
-
-  loginpage.enterUsername("Admin12");
-  loginpage.enterPassword("admin123");
-  loginpage.clickLogin();
-});
